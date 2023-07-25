4b3bcce04db8f74307bb4b110e3c3cb65d79324c
student <student@gnu.ac.in>
2023-07-25 08:53:34 +0530

    practical-1.10.kt

--- src/main/kotlin/practical-1.10.kt
+++ src/main/kotlin/practical-1.10.kt
@@ -1,18 +1,34 @@
-class student(val name:String){
+open class person(firstname:String,lastname:String){
+    val firstname=""
+    val lastname=""
+    var phoneno:String=""
+
+    constructor(firstname: String,lastname: String,phoneno:String):this(firstname,lastname)
+    var phone:String=phoneno
+}
+
+class student(fname:String,lname: String): person(firstname,lastname) {
+    var name:String= fname+" "+lname
     var enno:String="21012011103"
 
-    constructor(name: String,enrollment:String):this(name){
+    constructor(name: String,enrollment:String):this(fname,lname){
         enno=enrollment
         println("secondary constuctor called")
     }
 
-    init {
-        println("init block called")
-    }
+
+//    override fun toString(): String {
+//        return "$name , $enno"
+//    }
 
 }
 fun main(){
     val obj=student("rohan","21012011103")
+    println(obj)
+    println(obj.)
     println(obj.name)
-    println(obj.enno)
+    println(obj.lname)
+
+//    println(obj.name)
+//    println(obj.enno)
 }
